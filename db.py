6006bf4977f4f292b6e676dfda0a31ba7aa1327a
syleo5577 <sy.shuun.kim@gmail.com>
2023-11-05 16:59:16 +0900

    delete 추가, 주석 일부 수정

--- db.py
+++ db.py
@@ -4,7 +4,7 @@ import pickle
 import time
 
 # 메인 db 이름 형식
-# db_{학년 기수}.pkl (학년 기수를 나타내는 변수는 generation)
+# db_{학년 기수}.txt (학년 기수를 나타내는 변수는 generation, gen)
 
 # 메인 db 데이터 형식
 # {index} {str 영상 코드} {str 영상 제목} {int 영상 길이} {int 등록 (유닉스) 시간} {int 비활성화 여부} {int 삭제 여부}
@@ -16,28 +16,47 @@ import time
 
 # 기타 사항:
 
-def dbAppend(code, generation):
+def makeNewFile(path, p=True):
+    """만약 path의 경로에 파일이 없으면 빈 리스트가 들어있는 pkl 파일 만듦, p=False면 빈 파일 만듦
+
+    Args:
+        path (str): 파일 경로
+        p (bool): pickle 파일인지 아닌지 고름
+
+    Returns:
+        int: 0
+    """
+    try:
+        f = open(path, "rb")
+        f.close
+    except FileExistsError: # 만약 db 파일이 없으면 새 파일 만듦
+        if pickle:
+            with open(path, "wb") as fw:
+                pickle.dump([], fw)
+        else:
+            f = open(path, "w")
+            f.close()
+    
+    return 0
+
+def dbAppend(gen, code):
     """db에 영상 추가
 
     Args:
+        gen (int): 기수(期數)
         code (str): 유튜브 영상 코드
-        generation (int): 기수(期數)
 
     Returns:
-        int: 0: 추가 성공, 1: 중복, 2: 시간 초과, 3: 밴, 4: 실패(오류)
+        int: 0: 추가 성공, 1: 중복, 2: 시간 초과, 3: 차단됨, 4: 실패(오류)
     """
     
     try:
-        f = f"./db/db_{generation}.pkl"
+        path = f"./db/db_{gen}.pkl"
+        makeNewFile(path)
         
-        try:
-            with open(f,"rb") as fr:
-                lst : list = pickle.load(fr)
-        except FileExistsError: # 만약 db 파일이 없으면 새 파일 만듦
-            file = open(f, "w")
-            file.close
-            with open(f, "rb") as fr:
-                lst : list = pickle.load(fr)
+        # db 데이터 불러오기
+        with open(path,"rb") as fr:
+            lst : list = pickle.load(fr)
         
         # 나중에 여기에 중복 검사 추가 예정
         
@@ -46,12 +65,9 @@ def dbAppend(code, generation):
         if lenth > 600: # 10분 넘어가는 영상 거름
             return 2
         
-        # 밴 여부 검사
-        with open("./db/banned_list.pkl","rb") as fr:
-            banList : list = pickle.loads(fr)
-        for i in range(len(banList)):
-            if banList[i] == code:
-                return 3
+        # 차단 여부 검사
+        if ban(code) == 1:
+            return 3
         
         # (검사를 통과하면) db에 추가
         lst.append([len(lst), code, title, lenth, int(time.time()), 0, 0])
@@ -62,37 +78,78 @@ def dbAppend(code, generation):
 
     
 def ban(code):
-    """banned_list.pkl에 유튜브 영상 추가
+    """banned_list.txt에 유튜브 코드 영상 추가
 
     Args:
         code (str): 유튜브 영상 코드
 
     Returns:
-        int: 0이면 append 성공, 1이면 중복, 2면 실패(오류발생)
+        int: 0이면 append 성공, 1이면 중복, 2면 실패(오류)
     """
     
     try:
-        with open("./db/banned_list.pkl","rb") as fr:
-            banList : list = pickle.loads(fr)
+        # db 데이터 불러오기
+        f = open("db/banned_list.txt", "a")
+        banList = f.readlines()
         
+        # 중복검사
         for i in range(len(banList)):
             if banList[i] == code:
                 return 1
         
-        banList.append(code)
-        
-        with open("./db/banned_list.pkl", "wb") as fw:
-            pickle.dump(banList, fw)
+        f.writelines([code])
 
         return 0
     except:
         return 2
 
-def delete(i):
-    pass
+def delete(gen, i):
+    """db_{gen}.pkl에서 i번 인덱스의 영상을 삭제 여부를 1로 바꿈
 
-def deactivate(i):
-    pass
+    Args:
+        gen (int): 기수(期數)
+        i (int): index number
+
+    Returns:
+        int: 0; 성공, 1: 실패(오류)
+    """
+    try:
+        # db 데이터 불러오기
+        path = f"./db/db_{gen}.pkl"
+        makeNewFile(path)
+        with open(path, "rb") as fr:
+            lst : list = pickle.load(fr)
+        
+        # db 데이터 바꾸기
+        lst[i][6] = 1
+        
+        return 0
+    except:
+        return 1
+
+def deactivate(gen, i):
+    """db_{gen}.pkl에서 i번 인덱스의 비활성화 여부를 1로 바꿈
+
+    Args:
+        gen (int): 기수(期數)
+        i (int): index number
+
+    Returns:
+        _type_: 0: 성공, 1: 실패(오류)
+    """
+    try:
+        # db 데이터 불러오기
+        path = f"./db/db_{gen}.pkl"
+        makeNewFile(path)
+        with open(path, "rb") as fr:
+            lst : list = pickle.load(fr)
+        
+        # db 데이터 바꾸기
+        lst[i][5] = 1
+        
+        return 0
+    except:
+        return 1
 
 def callData():
     pass
