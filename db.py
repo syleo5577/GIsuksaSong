b6633a52fc0b190c7deec25283f9042dc27e221d
syleo5577 <sy.shuun.kim@gmail.com>
2023-11-05 15:49:56 +0900

    문서 삭제식 이동함

---
+++ db.py
@@ -0,0 +1,101 @@
+from datetime import datetime
+import link_functinos
+import pickle
+import time
+
+# 메인 db 이름 형식
+# db_{학년 기수(이하 generation 이라고 표현함)}.pkl
+
+# 메인 db 데이터 형식
+# {index} {str 영상 코드} {str 영상 제목} {int 영상 길이} {int 등록 (유닉스) 시간} {int 비활성화 여부} {int 삭제 여부}
+
+# 기본적인 기획:
+#   db에는 영상 코드, 제목, 길이, 등록한 시간, 비활성화 여부, 삭제 여부가 기록됨
+#   비활성화할 경우 노래가 재생되지는 않지만 비활성화됐다는 사실을 알 수 있음
+#   삭제될 경우 db에만 기록이 남으며 웹사이트에서는 확인할 수 없음
+
+# 기타 사항:
+
+def dbAppend(code, generation):
+    """db에 영상 추가
+
+    Args:
+        code (str): 유튜브 영상 코드
+        generation (int): 기수(期數)
+
+    Returns:
+        int: 0: 추가 성공, 1: 중복, 2: 시간 초과, 3: 밴, 4: 실패(오류)
+    """
+    
+    try:
+        f = f"./db/db_{generation}.pkl"
+        
+        try:
+            with open(f,"rb") as fr:
+                lst : list = pickle.load(fr)
+        except FileExistsError: # 만약 db 파일이 없으면 새 파일 만듦
+            file = open(f, "w")
+            file.close
+            with open(f, "rb") as fr:
+                lst : list = pickle.load(fr)
+        
+        # 나중에 여기에 중복 검사 추가 예정
+        
+        # 시간 검사
+        lenth, title = link_functinos.getLengthAndTitle(code)
+        if lenth > 600: # 10분 넘어가는 영상 거름
+            return 2
+        
+        # 밴 여부 검사
+        with open("./db/banned_list.pkl","rb") as fr:
+            banList : list = pickle.loads(fr)
+        for i in range(len(banList)):
+            if banList[i] == code:
+                return 3
+        
+        # (검사를 통과하면) db에 추가
+        lst.append([len(lst), code, title, lenth, int(time.time()), 0, 0])
+        return 0
+        
+    except:
+        return 4
+
+    
+def ban(code):
+    """banned_list.pkl에 유튜브 영상 추가
+
+    Args:
+        code (str): 유튜브 영상 코드
+
+    Returns:
+        int: 0이면 append 성공, 1이면 중복, 2면 실패(오류발생)
+    """
+    
+    try:
+        with open("./db/banned_list.pkl","rb") as fr:
+            banList : list = pickle.loads(fr)
+        
+        for i in range(len(banList)):
+            if banList[i] == code:
+                return 1
+        
+        banList.append(code)
+        
+        with open("./db/banned_list.pkl", "wb") as fw:
+            pickle.dump(banList, fw)
+
+        return 0
+    except:
+        return 2
+
+def delete(i):
+    pass
+
+def deactivate(i):
+    pass
+
+def callData():
+    pass
+
+if __name__ == "__main__":
+    pass
---
+++ link_functinos.py
@@ -0,0 +1,105 @@
+from googleapiclient.discovery import build
+import re
+
+def addHTTPS(url):
+    """입력받은 문자열에 https:// 없으면 넣어줌
+
+    Args:
+        url (str): 입력받은 문자열
+
+    Returns:
+        str, 'https://'가 추가된 URL
+    """
+    
+    https = re.compile("https{0, 1}://")
+    
+    if not https.match(url):
+        return "https://" + url
+    else:
+        return url
+    
+
+def getYouTubeVideoID(url):
+    """입력받은 URL이 'https://www.youtube.com/watch?v='로 시작하는지 확인 + 글자수 확인
+    즉, 유튜브 영상의 링크를 입력으로 받아야 1 출력
+    (예: https://www.youtube.com을 입력받으면 0 출력) 
+    ! 이 함수는 입력받은 문자열이 URL인지 아닌지 검사하지 않음
+    
+    Args:
+        url (str): 검사할 문자열
+
+    Returns:
+        int: 유튜브 영상이면 유튜브 영상 코드, 아니면 '0'을 리턴
+    """
+    
+    yt_regex = re.compile(r'(https?://)?(www\.)?'
+        '(youtube|youtu|youtube-nocookie)\.(com|be)/'
+        '(shorts/|watch\?v=|embed/|v/|.+\?v=)?([^&=%\?]{11})')
+    
+    regex_match = yt_regex.search(url)
+    if regex_match:
+        return regex_match.group(6)
+    else:
+        return "0"
+
+
+def getLengthAndTitle(code):
+    """유튜브 영상 코드 넣으면 영상 시간, 제목 반환함
+
+    Args:
+        code (str): 유튜브 영상 코드
+
+    Returns:
+        int, str: 영상 길이, 영상 제목
+    """
+    
+    f = open("./youtubeAPIkey.txt", "r")
+    api_key = f.readline().strip()
+    f.close()   
+    
+    youtube = build('youtube', 'v3', developerKey=api_key)
+    response1 = youtube.videos().list(
+        part='contentDetails', 
+        id=code
+    ).execute()
+    response2 = youtube.videos().list(
+        part='snippet',
+        id=code
+    ).execute()
+    
+    length = response1['items'][0]['contentDetails']['duration']
+    title = response2['items'][0]['snippet']['title']
+
+    return strTimeToInt(length), title
+
+def strTimeToInt(length):
+    """유튜브 API에서 받은 시간 형식(PT{hh}H{mm}M{ss}S)을 초단위로 바꿔줌
+
+    Args:
+        length (str): 유튜브 영상의 길이(유튜브 API가 출력하는 형식)
+
+    Returns:
+        int: 영상의 길이
+    """
+    
+    i = len(length) - 2
+    j = i + 1
+    cnt = 0
+    time = 0
+    while i > 0:
+        if not ('0' <= length[i] <= '9'):
+            time += int(length[i+1:j]) * (60 ** (0 if length[j] == "S" else (1 if length[j] == "M" else 2))) 
+            cnt += 1
+            j = i
+        i -= 1
+    
+    return time
+
+if __name__ == "__main__":
+    url = addHTTPS(input())
+    code = getYouTubeVideoID(url)
+    if code == "0":
+        print("NOT YOUTUBE VIDEO")
+    else:
+        print("code:", code)
+        print(getLengthAndTitle(code))
