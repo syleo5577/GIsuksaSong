f1266818a6e96affdfabb3e40792b9ce9ca44c86
syleo5577 <sy.shuun.kim@gmail.com>
2023-11-14 14:34:46 +0900

    오타수정

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
+from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 app = FastAPI()
+
+class Item(BaseModel):
+    code : int
+    name : str
+    real : Optional[bool] = None
+    
 
 @app.get("/")
 def master():
     return "Hello, world!"
 
 @app.get("/items/{item_id}")
-async def read_item(item_id):
+def read_item(item_id):
     return {"item_id": item_id}
+
+@app.put("/items/{item_id}")
+def update_item(item_id : int, name : Optional[str] = None):
+    return {"item_id" : int, Item}
