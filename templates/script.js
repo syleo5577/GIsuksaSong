7f93ce2d64dd4ade219d67f938f7d35d090a2650
syleo5577 <sy.shuun.kim@gmail.com>
2023-12-03 12:54:01 +0900

    뜯어고침

--- templates/script.js
+++ templates/script.js
@@ -1,183 +1,181 @@
-let player;
-let currentSongIndex = 0;
-let isAdmin = false;
+let currentURL = new URL(window.location.href);
+let searchParams = new URLSearchParams(currentURL.search);
+let generation = searchParams.get("gen");
 
 
-function onYouTubeIframeAPIReady() {
-    player = new YT.Player('videoPlayer', {
-        height: '315',
-        width: '560',
-        events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-        }
-    });
-}
 
-function onPlayerReady(event) {}
+/**
+ * 플레이리스트 채우는 함수. int는 db에서의 인덱스 말하는거임.
+ * @param {string} lowerText 
+ * @param {string} upperText 
+ * @param {string} imageUrl 
+ * @param {int} index 
+ */
+function addItemToList(upperText, lowerText, imageUrl, index) {
+    // itemContainer
+    let itemContainer = document.createElement('div');
+    itemContainer.classList.add('playlist-item');
+    itemContainer.classList.add('item-flex-container');
+    
+    
+    
+    // thumbnailContainer
+    let thumbnailContainer = document.createElement('div');
+    thumbnailContainer.classList.add('thumbnail-container');
+    
+    // thumbnail
+    let thumbnail = new Image();
+    thumbnail.src = imageUrl; // 이미지 경로를 실제 이미지 경로로 대체
+    thumbnailContainer.appendChild(thumbnail);
+    
 
-function onPlayerStateChange(event) {
-    if (event.data === YT.PlayerState.ENDED) {
-        playNextSong();
+    // textContainer
+    let textContainer = document.createElement('div');
+    textContainer.classList.add('text-container');
+    
+    // upperTexteliment
+    let upperTextElement = document.createElement('p');
+    upperTextElement.classList.add('upper-text-eliment');
+    upperTextElement.appendChild(document.createTextNode(upperText));
+    textContainer.appendChild(upperTextElement);
+
+    // lowerTexteliment
+    let lowerTextElement = document.createElement('p');
+    lowerTextElement.classList.add('lower-text-eliment');
+    lowerTextElement.appendChild(document.createTextNode(lowerText));
+    textContainer.appendChild(lowerTextElement);
+    
+    
+    // buttonContainer
+    let buttonsContainer = document.createElement('div');
+    buttonsContainer.classList.add('buttons-container');
+    
+    // buttonsUl
+    let buttonsUl = document.createElement('ul');
+    buttonsUl.classList.add('buttons-ul');
+
+    // buttonsLi
+    let buttonsLi = document.createElement('li');
+    buttonsLi.classList.add('buttons-li');
+
+    // buttons
+    textNodeArr = ['좋아요', '싫어요', 3, 4];
+    for (var i = 0; i < 2; i++){
+        let button = document.createElement('button');
+        button.classList.add('round-button');
+        button.classList.add('button' + (i+1));
+        button.id = 'button' + (i+1) + '-' + index;
+        button.appendChild(document.createTextNode(textNodeArr[i]));
+
+        buttonsLi.appendChild(button);
+        buttonsUl.appendChild(buttonsLi);
     }
+
+    buttonsContainer.appendChild(buttonsUl);
+    
+    
+    // 총합
+    itemContainer.appendChild(thumbnailContainer);
+    itemContainer.appendChild(textContainer);
+    itemContainer.appendChild(buttonsContainer);
+    
+    let liEliment = document.createElement('li');
+    liEliment.appendChild(itemContainer);
+
+    
+    let playlist = document.getElementById('playlist');
+    playlist.appendChild(liEliment);
+    
+    return 0;
 }
 
-function getVideoTitle(videoId, callback) {
-    const apiKey = 'YOUR_API_KEY'; // Replace with your own API key
-    const apiUrl = `https://www.googleapis.com/youtube/v3/videos?id=${videoId}&key=${apiKey}&part=snippet`;
+/**
+ * 
+ * @returns array[array[int, str, str, int * 6]]
+ */
+async function getData() {
+    let playlistArr;
 
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            if (data && data.items && data.items.length > 0) {
-                const title = data.items[0].snippet.title;
-                callback(title);
-            } else {
-                callback('Unknown Title');
+    try {
+        let response = await fetch(`/list/data?gen=${generation}`, { method: 'GET' });
+        let data = await response.json();
+        playlistArr = data.arr;
+    } catch (error) {
+        console.error('Error:', error);
+    }
+
+    console.log(playlistArr);
+    return playlistArr;
+}
+
+async function postLink(e) {
+    let input = document.getElementById('linkInput');
+    let inputValue = input.value;
+    if (e.keyCode == 13){
+        e.preventDefault();
+        
+        try {
+            const response = await fetch('/list?gen=0', {
+                method: 'POST', 
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({url: inputValue})
+            });
+    
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
             }
-        })
-        .catch(error => console.error('Error:', error));
-}
+    
+            const jsonData = await response.json();
+            console.log(jsonData);
 
-let playlist = [];
-
-function addSong() {
-    const youtubeLink = document.getElementById('youtubeLink').value;
-    const videoId = getVideoIdFromLink(youtubeLink);
-    if (videoId) {
-        getVideoTitle(videoId, (title) => {
-            playlist.push({ videoId, title });
-            document.getElementById('youtubeLink').value = '';
+            input = null;
+            fillList
             
-            if (playlist.length === 1) { 
-                currentSongIndex = 0;     
-                playNextSong();           
-            }
-
-            displayPlaylist();
-        });
+            return jsonData;
+        } catch (error) {
+            console.error('Error:', error);
+        }
     }
 }
 
 
+/**
+ * 
+ * @param {float | int} t 
+ * @returns yyyy-mm-
+ */
+function Unix_timestamp(t){
+    const date = new Date(t*1000);
 
+    // 원하는 날짜 및 시간 형식으로 변환하기
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
 
-function displayPlaylist() {
-    const playlistContainer = document.getElementById('playlist');
-    playlistContainer.innerHTML = '';
-
-    playlist.forEach((song, index) => {
-        const li = document.createElement('li');
-        
-        const img = document.createElement('img');
-        img.src = `https://img.youtube.com/vi/${song.videoId}/default.jpg`; // Get the thumbnail image URL
-        img.alt = `Thumbnail for Music No.${index + 1}`;
-        img.classList.add('thumbnail');
-        li.appendChild(img);
-        
-        const musicInfo = document.createElement('div');
-        musicInfo.classList.add('music-info');
-        
-        const span = document.createElement('span');
-        span.textContent = `Music No.${index + 1}`; // Display music name as "Music No.X"
-        musicInfo.appendChild(span);
-        
-        const buttonsContainer = document.createElement('div');
-        buttonsContainer.classList.add('buttons-container'); // Add class for styling
-        
-        // Add "Play" button
-        const playButton = document.createElement('button');
-        playButton.className = 'play-button';
-        playButton.textContent = 'Play';
-        playButton.onclick = () => playSong(index);
-        buttonsContainer.appendChild(playButton);
-        
-        // Add "Delete" button
-        const deleteButton = document.createElement('button');
-        deleteButton.className = 'delete-button';
-        deleteButton.textContent = 'Delete';
-        deleteButton.onclick = () => deleteSong(index);
-        buttonsContainer.appendChild(deleteButton);
-        
-        musicInfo.appendChild(buttonsContainer);
-        
-        li.appendChild(musicInfo);
-
-        playlistContainer.appendChild(li);
-    });
+    return `${year}-${String(month).padStart(2, "0")}-${day} ${hours}:${minutes}:${seconds}`;
 }
 
-// ... (The rest of your existing code)
-
-
-
-function playNextSong() {
-    const videoPlayerContainer = document.getElementById('videoPlayer');
-    if (currentSongIndex < playlist.length) { 
-        const videoId = playlist[currentSongIndex].videoId;
-        player.loadVideoById({ videoId, suggestedQuality: 'large', loop: false });
-        player.playVideo();
-        currentSongIndex++; 
-        displayPlaylist();
-    } else if(playlist.length > 0){
-        currentSongIndex=0;
-        playNextSong();
-    } else{
-        videoPlayerContainer.innerHTML='';
+/**
+ * 
+ * @returns 0
+ */
+async function fillList(){
+    let playlistArr = await getData();
+    let playlistLength = playlistArr.length;
+    for (let i = 0; i < playlistLength; i++) {
+        let index, code, title, videoLength, uploadTime, isDeactivated, isDeleted, likes, dislikes;
+        [index, code, title, videoLength, uploadTime, isDeactivated, isDeleted, likes, dislikes] = playlistArr[i];
+        addItemToList(title, Unix_timestamp(uploadTime), `https://i.ytimg.com/vi/${code}/hq720.jpg`, index);
     }
+
+    return 0
 }
 
-function playSong(index) {
-    const videoPlayerContainer = document.getElementById('videoPlayer');
-    const videoId = playlist[index].videoId;
-    player.loadVideoById({ videoId, suggestedQuality: 'large', loop: false });
-    player.playVideo();
-    displayPlaylist();
-}
-
-function deleteSong(index) {
-    const videoPlayerContainer = document.getElementById('videoPlayer');
-    const videoElement = videoPlayerContainer.querySelector('iframe');
-    if (videoElement) {
-        videoElement.remove(); 
-        player.stopVideo(); 
-    }
-    playlist.splice(index, 1);
-    displayPlaylist();
-}
-
-function deleteBatch() {
-    const videoPlayerContainer = document.getElementById('videoPlayer');
-    const videoElement = videoPlayerContainer.querySelector('iframe');
-    if (videoElement) {
-        videoElement.remove(); 
-        player.stopVideo(); 
-    }
-    playlist = [];
-    displayPlaylist();
-}
-
-
-
-
-
-
-if (evnet.keyCode == 13) {
-    document.getElementById('input').addEventListener('submit', function(e) {
-        e.preventDefault(); // 폼의 기본 제출 동작을 방지합니다.
-    
-        var inputValue = document.getElementById('myInput').value; // 입력 값을 가져옵니다.
-    
-        // 여기에서 서버로 데이터를 전송하는 코드를 작성합니다.
-        fetch('/main', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({url: inputValue}),
-        })
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch((error) => console.error('Error:', error));
-    });
-}
+document.addEventListener('DOMContentLoaded', async function () {
+    fillList();
+});
