35a8603f55280bdd051418c8d2ac892ad20fdad8
syleo5577 <sy.shuun.kim@gmail.com>
2023-12-07 20:08:11 +0900

    pep8 맞춤

--- db_functions.py
+++ db_functions.py
@@ -1,9 +1,9 @@
-import link_functions
-import moviepy.editor as mp
+import time
 import os
 import pickle
+import moviepy.editor as mp
 from pytube import YouTube
-import time
+import link_functions
 
 # 메인 db 이름 형식
 # db_{학년 기수}.pkl (학년 기수를 나타내는 변수는 generation, gen)
@@ -15,7 +15,8 @@ import time
 #   db에는 영상 코드, 제목, 길이, 등록한 시간, 다운로드 여부, 삭제 여부가 기록됨
 #   삭제할 경우 db에만 기록이 남으며 웹사이트에서는 확인할 수 없음
 
-def getData(gen : int):
+
+def get_data(gen: int):
     """db 데이터 호출
 
     Args:
@@ -24,7 +25,7 @@ def getData(gen : int):
     Returns:
         list: DB data
     """
-    
+
     dir = f"./db/db_{gen}.pkl"
     if os.path.isfile(dir):
         with open(dir, "rb") as fr:
@@ -34,10 +35,14 @@ def getData(gen : int):
         with open(dir, "wb") as fw:
             pickle.dump([], fw)
         
+        
+    # db 데이터 받은거 return
+
     # db 데이터 받은거 return
     return arr
 
-def getDataWithoutDeleted(gen : int):
+
+def get_data_without_deleted(gen: int):
     """db에서 삭제되지 않은 데이터 호출
 
     Args:
@@ -47,20 +52,21 @@ def getDataWithoutDeleted(gen : int):
         list: DB data without deleted index
     """
 
-    arr = getData(gen)
-    
-    # db 데이터 받은거 arr[n][6](삭제 여부)=0인거만 newarr에 넣음
-    newarr = []
-    l = len(arr)
-    for i in range(l):
-        if arr[i][6] == 0:
-            newarr.append(arr[i])
-        
-    return newarr
+    arr = get_data(gen)
 
-def setData(gen : int, data : list):
+    # db 데이터 받은거 arr[n][6](삭제 여부)=0인거만 newarr에 넣음
+    new_arr = []
+    length = len(arr)
+    for i in range(length):
+        if arr[i][6] == 0:
+            new_arr.append(arr[i])
+
+    return new_arr
+
+
+def set_data(gen: int, data: list):
     """db 데이터 갱신
-    
+
     Args:
         gen (int): generation
         data (anything): updated db data
@@ -68,17 +74,18 @@ def setData(gen : int, data : list):
     Returns:
         int: 0
     """
-    
+
     # 경로 설정
     path = f"./db/db_{gen}.pkl"
-    
+
     # db 데이터 갱신
     with open(path, "wb") as fw:
         pickle.dump(data, fw)
-    
+
     return 0
 
-def dbAppend(gen : int, code : str):
+
+def db_append(gen: int, code: str):
     """db에 영상 추가
 
     Args:
@@ -88,45 +95,47 @@ def dbAppend(gen : int, code : str):
     Returns:
         str: 작동 성공/실패 여부. 등록 성공시 영상 정보도 함께 보냄.
     """
-    
-    try:
-        arr = getData(gen)
-        
+
+    # try:
+    if True:
+        arr = get_data(gen)
+
         # 시간 검사
-        lenth, title = link_functions.getLengthAndTitle(code)
-        if lenth > 600: # 10분 넘어가는 영상 거름
+        lenth, title = link_functions.get_length_and_title(code)
+        if lenth > 600:  # 10분 넘어가는 영상 거름
             return "timeout", [0, '', '', 0, 0, 0, 0, 0, 0]
-        
+
         # 차단 여부 검사
         if os.path.isfile(f"db/ban_{gen}.pkl"):
             with open(f"db/ban_{gen}.pkl", "rb") as fr:
-                banDict = pickle.load(fr)
-            if code in banDict:
+                ban_dict = pickle.load(fr)
+            if code in ban_dict:
                 return "banned", [0, '', '', 0, 0, 0, 0, 0, 0]
-        
+
         # 중복 검사
-        isDuplicated = False
-        l = len(arr)
-        for i in range(l):
+        is_duplicated = False
+        length = len(arr)
+        for i in range(length):
             if (arr[i][1] == code) and (arr[i][5] == 0) and (arr[i][6] == 0):
-                isDuplicated = True
+                is_duplicated = True
                 break
             else:
                 continue
-        
-        if isDuplicated:
-            return "duplicated", [0, '', '', 0, 0, 0, 0, 0, 0]
-        
-        # (검사를 통과하면) db에 추가
-        arrToAppend = [len(arr), code, title, lenth, int(time.time()//1), 0, 0, 0, 0]
-        arr.append(arrToAppend)
-        setData(gen, arr)
-        
-        return "success", arrToAppend
-    except:
-        return "runtime error", [0, '', '', 0, 0, 0, 0, 0, 0]
 
-def ban(gen : int, i : int, code : str):
+        if is_duplicated:
+            return "duplicated", [0, '', '', 0, 0, 0, 0, 0, 0]
+
+        # (검사를 통과하면) db에 추가
+        arr_to_append = [len(arr), code, title, lenth, int(time.time()//1), 0, 0, 0, 0]
+        arr.append(arr_to_append)
+        set_data(gen, arr)
+
+        return "success", arr_to_append
+    # except:
+    #     return "runtime error", [0, '', '', 0, 0, 0, 0, 0, 0]
+
+
+def ban(gen: int, i: int, code: str):
     """ban_{gen}.pkl에 유튜브 영상 코드 추가
 
     Args:
@@ -137,27 +146,29 @@ def ban(gen : int, i : int, code : str):
     Returns:
         str: 실행 결과
     """
-    
-    try:
+
+    # try:
+    if True:
         # db 데이터 불러오기
         dir = f"db/ban_{gen}.pkl"
         if os.path.isfile(dir):
             with open(dir, "rb") as fr:
-                banDict = pickle.load(fr)
+                ban_dict = pickle.load(fr)
         else:
-            banDict = dict()
-        
-        banDict[code] = 1
-        
+            ban_dict = dict()
+
+        ban_dict[code] = 1
+
         # db 업데이트
         with open(dir, "wb") as fw:
-            pickle.dump(banDict, fw)
-        
-        return 'success'
-    except:
-        return 'runtime error'
+            pickle.dump(ban_dict, fw)
 
-def deactivate(gen : int, i : int):
+        return 'success'
+    # except:
+    #     return 'runtime error'
+
+
+def deactivate(gen: int, i: int):
     """db_{gen}.pkl에서 i번 인덱스의 비활성화 여부를 1로 바꿈
 
     Args:
@@ -169,20 +180,22 @@ def deactivate(gen : int, i : int):
             0: success
             1: runtime error
     """
-    
-    try:
+
+    # try:
+    if True:
         # db 데이터 불러오기
-        arr = getData(gen)
-        
+        arr = get_data(gen)
+
         # db 데이터 바꾸기
         arr[i][5] = 1
-        setData(gen, arr)
-        
-        return 0
-    except:
-        return 1
+        set_data(gen, arr)
 
-def delete(gen : int, i : int):
+        return 0
+    # except:
+    #     return 1
+
+
+def delete(gen: int, i: int):
     """db_{gen}.pkl에서 i번 인덱스의 영상을 삭제 여부를 1로 바꿈
 
     Args:
@@ -192,20 +205,22 @@ def delete(gen : int, i : int):
     Returns:
         str: 실행 결과
     """
-    
-    try:
+
+    # try:
+    if True:
         # db 데이터 불러오기
-        arr = getData(gen)
-        
+        arr = get_data(gen)
+
         # db 데이터 바꾸기
         arr[i][6] = 1
-        setData(gen, arr)
-        
-        return 'success'
-    except:
-        return 'runtime error'
+        set_data(gen, arr)
 
-async def downloadVideo(code : str):
+        return 'success'
+    # except:
+    #     return 'runtime error'
+
+
+async def downloadVideo(code: str):
     """유튜브의 code 영상 다운로드. 이미 있으면 다운로드 안함
 
     Args:
@@ -215,19 +230,20 @@ async def downloadVideo(code : str):
     Returns:
         str: dir path of downloaded file, if fail return 'error'
     """
-    
-    try:
+
+    # try:
+    if True:
         # 파일 존재 여부 확인
         mp4_file = f"db/mp4s/{code}.mp4"
         mp3_file = f"db/mp3s/{code}.mp3"
         if os.path.isfile(mp3_file):
             return mp3_file
-    
+
         # 유튜브 영상 다운로드
         url = f"https://youtube.com/watch?v={code}"
         yt = YouTube(url)
         video = yt.streams.filter(only_audio=True).first()
-        video.download(output_path=f'db/mp4s', filename=code+'.mp4')
+        video.download(output_path='db/mp4s', filename=code+'.mp4')
 
         # 다운로드한 영상을 mp3로 변환
         clip = mp.AudioFileClip(mp4_file)
@@ -236,19 +252,19 @@ async def downloadVideo(code : str):
         # 변환 후 mp4 파일 삭제
         clip.close()
         os.remove(mp4_file)
-        
+
         return mp3_file
-    except:
-        return "runtime error"
+    # except:
+    #     return "runtime error"
 
 if __name__ == "__main__":
     # with open('db/ban_0.pkl', 'wb') as f:
     #     d = dict()
     #     pickle.dump(d ,f)
-    
+
     with open('db/ban_0.pkl', 'rb') as f:
         d = pickle.load(f)
         print(d)
-    
+
     # setData(1, [[0, 'gX9m-rCtSqc', '【Lyric Video】結束バンド「忘れてやらない」／ TVアニメ「ぼっち・ざ・ろっく！」第12話劇中曲', 218, 1198508400, 0, 0, 0, 0]])
-    print(getData(0))
+    print(get_data(0))
